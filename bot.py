4416b4265956448565ff25d1fd1b4489f3a75099
LaYellow <48283030+LaYellow@users.noreply.github.com>
2019-03-23 11:24:35 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ async def change_status():
 
     while not client.is_closed:
         current_status = next(msgs)
-        await client.change_presence(game=discord.Game(name =current_status))
+        await client.change_presence(game=discord.Game(name =current_status, type = 1))
         await asyncio.sleep(5)
 
 @client.event
